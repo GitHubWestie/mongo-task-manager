f6abe00987f568f2f51fb863c840b46e31233b9e
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-06-19 13:47:18 +0000

    Finished Lesson 12 - Logout Functionality

--- app.py
+++ app.py
@@ -87,7 +87,20 @@ def profile(username):
     # ["username"] at the end of this line means only the username is retrieved from the db.
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 
 if __name__ == "__main__":
