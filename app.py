bfa206bf249bff9b928fe9c3f8802dd69f3cb6a5
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-06-19 12:21:35 +0000

    Finished Lesson 11 - Display User Profile Page

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -67,6 +68,7 @@ def login():
                 # if password matches creates session cookie and displays flash message
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Username and/or password incorrect")
@@ -77,6 +79,15 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+# app.route takes username variable inside angle brackets. Then passed into function.
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # ["username"] at the end of this line means only the username is retrieved from the db.
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
